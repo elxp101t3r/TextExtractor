8cd4cbf1a563af8573d4fba5985807d6ba81f798
elxp101t3r <eliotx10101@outlook.com>
2024-02-18 17:35:38 +0200

    GUI Window

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from PIL import Image
 from pytesseract import pytesseract
+from kivy.core.window import Window
 
+
+Window.size= (500, 500)
+
+
+class ExtractingTextApp(MDApp):
+    def build(self):
+        self.layout = MDRelativeLayout(md_bg_color=[248/255, 200/255, 220/255])
+        
+        return self.layout
+
+
+if __name__ == "__main__":
+    ExtractingTextApp().run()
