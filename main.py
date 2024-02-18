c485b88e01e41fa02bf69131d5806534c455c5f2
elxp101t3r <eliotx10101@outlook.com>
2024-02-18 17:14:08 +0200

    Init

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivymd.app import MDApp
+from kivymd.uix.relativelayout import MDRelativeLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from PIL import Image
+from pytesseract import pytesseract
+
